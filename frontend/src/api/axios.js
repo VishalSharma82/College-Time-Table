ef086e694b9de3e065ebcfa5efddce2539b0c5a4
Vishal sharma <vishalsharma42619@gmail.com>
2025-09-28 19:14:39 +0530

    final

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://college-time-table-api.onrender.com/',
+  baseURL: 'https://college-time-table-api.onrender.com',
   withCredentials: true,
 });
 
